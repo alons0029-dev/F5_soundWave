9a5ae84400d4be78346e8c86afa8f5e7bbecf77e
alons0029-dev <alons0029.dev@gmail.com>
2024-10-23 12:43:03 +0200

    pageFooter started, NOT FINISHED

---
+++ src/components/pageFooter/PageFooter.jsx
@@ -0,0 +1,16 @@
+import './pageFooter.css';
+
+function PageHeader() {
+    return (
+      <>
+        <footer>
+            <section className="contact-info">
+                
+            </section>
+            <section className="social-networks"></section>
+        </footer>
+      </>
+    )
+  }
+  
+  export default PageHeader
