15f56902815d3c754811a9f9f03aa6b6e60ea01e
alons0029-dev <alons0029.dev@gmail.com>
2024-10-25 12:06:21 +0200

    Info PDF files added, tool added in README.md

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,10 @@
 
 import PageHeader from "./components/pageHeader/PageHeader";
-import PageFooter from "./components/pageFooter/PageFooter";
 
 function App() {
   return (
     <>
       <PageHeader />
-      <PageFooter />
     </>
   )
 }
