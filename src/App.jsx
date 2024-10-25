512e17309616572a6c27c0a60f14af4391bd33a8
alons0029-dev <alons0029.dev@gmail.com>
2024-10-25 12:42:06 +0200

    Main Page structure finished

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 
 import PageHeader from "./components/pageHeader/PageHeader";
+import PageHome from "./components/pageHome/pageHome";
 
 function App() {
   return (
     <>
       <PageHeader />
+      <PageHome />
     </>
   )
 }
