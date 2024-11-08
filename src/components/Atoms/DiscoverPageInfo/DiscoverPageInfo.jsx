db75f311a8a0df8891f3cae55ff6bcc7a8b8b169
alons0029-dev <alons0029.dev@gmail.com>
2024-11-08 11:10:35 +0100

    Discover components created, working on it

---
+++ src/components/Atoms/DiscoverPageInfo/DiscoverPageInfo.jsx
@@ -0,0 +1,14 @@
+
+// Style
+import "./discover-page-info.css";
+
+/* FUNCTIONALITY */
+const DiscoverPageInfo = () => {
+  return (
+    <div className="discover-page-info-container">
+        <p className="discover-page-info-text">By joining you can benefit by listening to the latest albums released.</p>
+    </div>
+  )
+}
+
+export default DiscoverPageInfo
---
+++ src/components/Atoms/DiscoverPageTitle/DiscoverPageTitle.jsx
@@ -0,0 +1,14 @@
+
+// Style
+import "./discover-page-title.css";
+
+/* FUNCTIONALITY */
+const DiscoverPageTitle = () => {
+  return (
+    <div className="discover-page-title-container">
+        <p className="discover-page-title-text">Discover new music</p>
+    </div>
+  )
+}
+
+export default DiscoverPageTitle
